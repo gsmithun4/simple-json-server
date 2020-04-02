277475a6afb3568a256421d131f019e4619306af
Midhun G S <midhun.gs.contractor@move.com>
2020-04-03 00:40:53 +0530

    fix

--- bin/global-server.js
+++ bin/global-server.js
@@ -3,10 +3,11 @@ const appStarter = require('../lib');
 
 const args = process.argv.splice(process.execArgv.length + 2);
 let file = `${process.cwd()}\\db.json`;
+let port = 3000;
 if (args && args.length > 0) {
 	args.map((arg, index) => {
 		if (arg === '-p')
-			port = args[index + 1] || 3000;
+			port = args[index + 1];
 		else if (arg === '-f')
 			file = `${process.cwd()}\\${args[index + 1]}`;
 		else if (arg === '-fp') 
